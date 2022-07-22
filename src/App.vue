88ccdabfec2b999d865e155c5f2b04f5c9b2c61d
klausMistlberger <klaus.mistlberger@pm.me>
2022-07-23 01:04:00 +0200

    error refinement

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,12 @@ const dataUrl = 'https://zoo-animal-api.herokuapp.com/animals/rand/5';
 const animals = [];
 const dataLoaded = ref(false);
 const fetchError = ref(false);
+const errorMessage = ref('Loading data...');
+const counter = ref(0);
 
 const fetchData = async () => {
   const response = await fetch(dataUrl);
-  if (response && response.statusText === 'OK') {
+  if (response && response.ok) {
     const data = await response.json();
     data.forEach((element, index) => animals.push({ ...element, ix: index }));
     dataLoaded.value = true;
@@ -22,11 +24,19 @@ const fetchData = async () => {
 
 const fetchDataErrorBoundaries = async () => {
   fetchError.value = false;
+  errorMessage.value = null;
   try {
     await fetchData();
   } catch (error) {
     fetchError.value = true;
+    errorMessage.value = error.message;
   }
+};
+
+const clickHandler = () => {
+  fetchDataErrorBoundaries();
+  counter.value++;
+  console.log(counter.value);
 };
 
 fetchDataErrorBoundaries();
@@ -36,29 +46,43 @@ fetchDataErrorBoundaries();
   <Header />
   <Slider v-if="dataLoaded" :data="animals" />
   <div v-else>
-    <div class="center" v-if="!fetchError">Loading data...</div>
-    <div class="message" v-else>
-      <div class="center">Something went wrong...</div>
-      <button class="tryagain" @click="fetchDataErrorBoundaries">
-        Try again
-      </button>
+    <div v-if="counter < 5">
+      <div class="message-container" v-if="!fetchError">
+        <div class="message">Loading data...</div>
+      </div>
+      <div class="message-container" v-else>
+        <div class="message">{{ errorMessage }}</div>
+        <button class="tryagain" @click="clickHandler">Try again</button>
+      </div>
+    </div>
+    <div v-else>
+      <div class="message--container">
+        <div class="message unavailable">
+          <p>Service currently unavailable.</p>
+          <p>Please try again later.</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-.center {
-  margin: 16px;
-  text-align: center;
+.message-container {
+  display: flex;
+  flex-direction: column;
 }
 
 .message {
-  display: flex;
-  flex-direction: column;
+  margin: 16px;
+  text-align: center;
 }
 
 .tryagain {
   width: 180px;
   margin: 0 auto;
+}
+
+.unavailable {
+  color: purple;
 }
 </style>
